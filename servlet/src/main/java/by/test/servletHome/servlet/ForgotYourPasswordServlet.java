e42cb360d35b342df98c2daed154f14f27be17fe
Kris <kristingallagher69@gmail.com>
2025-07-16 12:51:02 +0300

    learning

---
+++ servlet/src/main/java/by/test/servletHome/servlet/ForgotYourPasswordServlet.java
@@ -0,0 +1,42 @@
+package by.test.servletHome.servlet;
+
+import by.test.servletHome.storage.InMemoryUserStorage;
+import by.test.servletHome.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/forgot")
+public class ForgotYourPasswordServlet extends HttpServlet {
+
+    private final InMemoryUserStorage inMemoryUserStorage = new InMemoryUserStorage();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        User user = inMemoryUserStorage.getByLogin(login);
+        req.setAttribute("question", user.getQuestion());
+        getServletContext().getRequestDispatcher("/pages/forgot.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String answer = req.getParameter("answer");
+        String newPassword = req.getParameter("password");
+        User byLogin = inMemoryUserStorage.getByLogin(login);
+        if (byLogin != null) {
+            if (byLogin.getAnswer().equals(answer)) {
+                byLogin.setPassword(newPassword);
+                resp.sendRedirect("/auth");
+            } else {
+                req.setAttribute("messageAnswer", "Answer is not correct.");
+                getServletContext().getRequestDispatcher("/pages/forgot.jsp").forward(req, resp);
+            }
+        }
+    }
+}
---
+++ servlet/src/main/java/by/test/servletHome/storage/InMemoryUserStorage.java
@@ -0,0 +1,40 @@
+package by.test.servletHome.storage;
+
+import by.test.servletHome.model.User;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryUserStorage {
+    private static final List<User> users = new ArrayList<>();
+
+    public boolean save(User user) {
+        if (users.contains(user)) {
+            return false;
+        }
+        users.add(user);
+        return true;
+    }
+
+    public User getByLogin(String login) {
+        for (User user : users) {
+            if (user.getLogin().equals(login)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public boolean passwordLength(String password) {
+        if (password.length() > 3 && password.length() < 10) {
+            return true;
+        }
+        return false;
+    }
+
+    /*public User uniqueLogin(String login){
+
+    }*/
+
+}
